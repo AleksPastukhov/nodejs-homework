8d8062ca00f7aa1f2c2f7a1e5f9be6c8e5559257
AleksPastukhov <pastukhov.os@icloud.com>
2023-05-20 12:40:17 +0300

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+// aleks;
+// kJ5hdBBRtqRRlBuF;
+const DB_HOST =
+  "mongodb+srv://aleks:kJ5hdBBRtqRRlBuF@cluster0.maspzch.mongodb.net/db_contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
